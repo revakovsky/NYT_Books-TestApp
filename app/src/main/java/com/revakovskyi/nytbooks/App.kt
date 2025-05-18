48e55c4df94b1b158fb6859d4df417fa50d077ae
m.revakovskyi <m.revakovskyi@pin-up.tech>
2025-05-18 19:05:13 +0300

    add an Application class, update an app icon, move MainActivity to the presentation folder

---
+++ app/src/main/java/com/revakovskyi/nytbooks/App.kt
@@ -0,0 +1,11 @@
+package com.revakovskyi.nytbooks
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
--- app/src/main/java/com/revakovskyi/nytbooks/presentation/MainActivity.kt
+++ app/src/main/java/com/revakovskyi/nytbooks/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.revakovskyi.nytbooks
+package com.revakovskyi.nytbooks.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
