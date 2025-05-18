1129366de662b2df1ae5b46536499b28a6728e8a
m.revakovskyi <m.revakovskyi@pin-up.tech>
2025-05-18 20:20:10 +0300

    add a splash screen and its logo

--- app/src/main/java/com/revakovskyi/nytbooks/presentation/MainActivity.kt
+++ app/src/main/java/com/revakovskyi/nytbooks/presentation/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.revakovskyi.core.presentation.theme.NYTBooksTheme
 import com.revakovskyi.nytbooks.navigation.AppNavGraph
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                // TODO: add a condition!
+                false
+            }
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
